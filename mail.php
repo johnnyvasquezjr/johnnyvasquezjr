ca430b352ca99f32fad0bfeabe74530b9208dbb6
johnnyvasquezjr <johnny@thedigitalarchitect.io>
2026-02-08 21:05:17 -0600

    updated php

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
 $subject    = 'E-mail from example'; // Subject of your email
-$to         = 'email@example.com'; //Your e-mail address
+$to         = 'johnny@johnnyvasquezjr.co'; //Your e-mail address
 $headers    = 'MIME-Version: 1.0' . "\r\n" .
               'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 $message    = 'Name: ' . $_POST['name'] . ' <br/>' .
